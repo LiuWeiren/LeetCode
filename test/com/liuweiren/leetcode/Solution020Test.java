0a265b7326fd084a44e99919339df8afcb837d11
LiuWeiren <31265221+LiuWeiren@users.noreply.github.com>
2020-03-19 16:04:33 +0800

    Update Test

--- test/com/liuweiren/leetcode/Solution020Test.java
+++ test/com/liuweiren/leetcode/Solution020Test.java
@@ -13,20 +13,16 @@ class Solution020Test {
         int[] vals = {1, 2, 3, 4, 5};
 
         Solution020.ListNode head = solution.new ListNode(vals[0]);
-        Solution020.ListNode head2 = solution.new ListNode(vals[1]);
-        head.next = head2;
-        Solution020.ListNode head3 = solution.new ListNode(vals[2]);
-        head2.next = head3;
-        Solution020.ListNode head4 = solution.new ListNode(vals[3]);
-        head3.next = head4;
-        head4.next = solution.new ListNode(vals[4]);
-
-        // while (head != null) {
-        //     System.out.println(head.val);
-        //     head = head.next;
-        // }
-
+        Solution020.ListNode headTemp = head;
+        for (int i = 1; i < vals.length; i++) {
+            headTemp.next = solution.new ListNode(vals[i]);
+            headTemp = headTemp.next;
+        }
 
         Solution020.ListNode node = solution.reverseList(head);
+        while (node != null) {
+            System.out.print(node.val + ", ");
+            node = node.next;
+        }
     }
 }
