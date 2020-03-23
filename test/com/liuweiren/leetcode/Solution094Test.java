94e5786960c7f8738aae12bf33c1c21d6f930ad9
LiuWeiren <31265221+LiuWeiren@users.noreply.github.com>
2020-03-23 19:09:50 +0800

    945

---
+++ test/com/liuweiren/leetcode/Solution094Test.java
@@ -0,0 +1,16 @@
+package com.liuweiren.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+class Solution094Test {
+
+    Solution094 solution = new Solution094();
+
+    @Test
+    void minIncrementForUnique() {
+
+        int[] a = {3 ,2 ,1 ,2 ,1 ,7};
+
+        System.out.println(solution.minIncrementForUnique(a));
+    }
+}
